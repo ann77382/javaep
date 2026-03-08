c72cfff4b4ec1b3064b3c1da346ff63b6262f5a7
ann77382 <145916799+ann77382@users.noreply.github.com>
2026-03-09 00:25:31 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
         System.out.println("Hello World");
         System.out.println("This is DevOps");
         System.out.println("This is Updated Multiple Triggers");
-        System.out.println("This is Updated Multiple Triggers")
+        System.out.println("This is Updated Multiple Triggers");
         
     }
 }
