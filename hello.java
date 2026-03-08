5c42b8226802468df4ecfc75e7cf5eee8b290dfd
ann77382 <145916799+ann77382@users.noreply.github.com>
2026-03-08 23:08:47 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,5 @@ public class hello{
     public static void main(String [] args){
         System.out.println("Hello World");
         System.out.println("This is DevOps");
-        System.out.println(" Updated This is DevOps");
-        System.out.println(" Updated This is DevOps");
-        System.out.println("Hello Jenkins");
     }
 }
